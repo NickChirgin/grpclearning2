a3ce1628c7cb675d956e6da284cc03196bc4fff8
Nikolay <nickchirgin@gmail.com>
2023-03-10 22:40:41 +0300

    added mongodb connection

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/nickchirgin/grpclearning2/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
+
+var collection *mongo.Collection
 
 type server struct {
 	blogpb.UnimplementedBlogServiceServer
 }
+
+type item struct {
+	ID primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string `bson:"author_id"`
+	Content string `bson:"content"`
+	Title string `bson:"title"`
+}
 func main(){
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	fmt.Println("Blog service server started")	
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:qwerty@172.17.0.2:27017"))
+	if err != nil { log.Fatalf("Error while connecting to db: %v" ,err) }
+	collection = client.Database("grpc_test").Collection("blog")
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("failed to listen %v", err)
@@ -36,4 +56,5 @@ func main(){
 	s.Stop()
 	lis.Close()
 	fmt.Println("End of program")
+	client.Disconnect(context.TODO())
 }
